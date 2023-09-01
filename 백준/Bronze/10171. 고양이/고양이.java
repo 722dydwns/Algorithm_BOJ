6afd2de8ccd2d2eddfcaf0717f7b4c797dbb7b6d
YongJoOoO <39732720+YongJoOoO@users.noreply.github.com>
2023-09-01 18:46:52 +0900

    [Bronze V] Title: 고양이, Time: 120 ms, Memory: 13904 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,15 @@
+public class Main {	
+	//실행 메인 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		Scanner kb = new Scanner(System.in);
+
+		//출력
+	System.out.println( "\\    /\\\n" + 
+			" )  ( ')\n" + 
+			"(  /  )\n" + 
+			" \\(__)|");
+	}
+	
+}
