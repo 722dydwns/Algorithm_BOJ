cd68508d76c632062794a14d4d0bdd35a5d6ec71
YongJoOoOoOoO <39732720+YongJoOoOoOoO@users.noreply.github.com>
2023-09-04 12:57:11 +0900

    [Bronze II] Title: 상수, Time: 212 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class Main {
+	
+	//실행 메인
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner in = new Scanner(System.in);
+        
+		int A = in.nextInt();
+		int B = in.nextInt();
+        
+        in.close();
+        
+		A = Integer.parseInt(new StringBuilder().append(A).reverse().toString());
+		B = Integer.parseInt(new StringBuilder().append(B).reverse().toString());
+		
+		System.out.print(A > B ? A : B);
+		
+	}
+}
