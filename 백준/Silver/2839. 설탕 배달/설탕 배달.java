4f672660cdfec889f55ec2706449b272a40775ab
YongJoOoOoOoO <39732720+YongJoOoOoOoO@users.noreply.github.com>
2023-09-04 12:57:01 +0900

    [Silver IV] Title: 설탕 배달, Time: 212 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner kb = new Scanner(System.in);
+		int N = kb.nextInt();
+		int cnt= 0;
+		
+		while(true) {
+			if(N % 5 ==0) {
+				cnt += N/5;
+				break;
+			}
+			
+			if(N < 3 ) {
+				cnt = -1;
+				break;
+			}
+			N -= 3;
+			cnt++;
+		}
+		System.out.println(cnt);
+	}
+}
