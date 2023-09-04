03e7ae535b29238ae271734f5021d8c612c8a883
YongJoOoOoOoO <39732720+YongJoOoOoOoO@users.noreply.github.com>
2023-09-04 12:44:47 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 1924 ms, Memory: 223796 KB -BaekjoonHub

---
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+/*1937번. 욕심쟁이 판다- DP & DFS*/
+public class Main {
+	static int N;
+	static int[][] map;
+	static int[][] dp;
+	static int[] dx = {0, 0, 1, -1};
+	static int[] dy = {1, -1, 0, 0};
+	
+	//DFS
+	static int DFS(int x, int y) {
+		if(dp[x][y] != 0) return dp[x][y];
+		
+		dp[x][y] = 1;
+		for(int i=0; i<4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			if(nx<0 || ny<0 || nx >=N || ny>= N) continue;
+			if(map[x][y] < map[nx][ny]) {//직전 값보다 더 큰 값으로만 이동이 가능하기 때문
+				//기존 값과 nx로 깊이 탐색해서 반환되는 값 + 1
+				dp[x][y] = Math.max(dp[x][y], DFS(nx, ny) + 1);
+			}
+		}
+		return dp[x][y];
+	}
+	
+	//실행메인
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner kb= new Scanner(System.in);
+		N = kb.nextInt();
+		map = new int[N][N];
+		dp = new int[N][N];
+		
+		//데이터 입력
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<N; j++) {
+				map[i][j] = kb.nextInt();//차례로 입력받기 
+			}
+		}
+		int max = 0;
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<N; j++) {
+				max = Math.max(DFS(i, j), max);
+			}
+		}
+		
+		System.out.println(max);
+	}
+}
