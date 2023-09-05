e5e462e4706c06e09b3421427f62bcce4ebce86c
YongJoOoOoOoO <39732720+YongJoOoOoOoO@users.noreply.github.com>
2023-09-05 18:18:53 +0900

    [Silver I] Title: 음식물 피하기, Time: 376 ms, Memory: 29940 KB -BaekjoonHub

--- 백준/Silver/1743. 음식물 피하기/음식물 피하기.java
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.java
@@ -1,38 +1,31 @@
-import java.util.*;
+import java.util.Scanner;
 
 /*1743번. 음식물 피하기 - DFS &BFS*/
 public class Main {	
 	static int N, M, K;
 	static int[][] map;
 	static boolean[][] visited;
+	static int count;
 	
 	//4방향 
 	static int[] dx = {0, 0, 1, -1};
 	static int[] dy = {1, -1, 0, 0};
-	
-	//BFS
-	static int BFS(int x, int y) {
-		Queue<int[]> Q = new LinkedList<>();
-		int cnt = 0; //시작할 때 1로 세팅해서 
+	//DFS
+	static void DFS(int x, int y) {
+		count++;//재귀 호출될 때마다  +1 처리
 		visited[x][y] = true;
-		Q.offer(new int[] {x, y});
 		
-		while(!Q.isEmpty()) {
-			int[] cur = Q.poll();
-			cnt++;
-			for(int k=0; k<4; k++) {
-				int nx = cur[0] + dx[k];
-				int ny = cur[1] + dy[k];
-				if(nx <0 || ny <0 || nx >=N || ny>=M ) continue;
-				
-				if(!visited[nx][ny] && map[nx][ny] == 1) {//1이 나타나면 
-					Q.offer(new int[] {nx, ny});
-					visited[nx][ny] = true;
-				}
+		for(int k=0; k<4; k++) {
+			int nx = x + dx[k];
+			int ny = y + dy[k];
+			if(nx <0 || ny <0 || nx >= N || ny>= M) continue;
+			if(!visited[nx][ny] && map[nx][ny] == 1) {
+				DFS(nx, ny);//더 깊이 탐색하면서 나아감 
 			}
 		}
-		return cnt;
+		
 	}
+
 	//실행 메인 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -54,7 +47,9 @@ public class Main {
 		for(int i=0; i<N; i++) {
 			for(int j=0; j<M; j++) {
 				if(map[i][j] == 1) {
-					max = Math.max(max, BFS(i, j));
+					count = 0;//여기서 0으로 초기화 해두고 
+					DFS(i, j); //시작하지
+					max = Math.max(max, count);
 				}
 			}
 		}
