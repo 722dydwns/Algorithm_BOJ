b47938868889324787097a2d7fa31bb7b32bae2c
YongJoOoOoOoO <39732720+YongJoOoOoOoO@users.noreply.github.com>
2023-09-07 10:26:06 +0900

    [Silver V] Title: 수들의 합, Time: 216 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Silver/1789. 수들의 합/수들의 합.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+/*백준 1789번. 수들의 합 - 그리디 문풀 */
+public class Main {
+	
+	//실행 메인 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner kb= new Scanner(System.in);
+		 long S = kb.nextLong();
+		 
+		 int N= 0;
+		 long sum = 0L;
+		 int i=1;
+		 while(true) {
+			 sum += i;
+			 i++;
+			 if(sum > S) {
+				 break;
+			 }
+			 N++;
+		 }
+		 System.out.println(N);
+	}
+}
