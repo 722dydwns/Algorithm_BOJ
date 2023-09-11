b7e2c1a2e924dfcfa448736fd0c8a2cd566f28c3
YongJuu <39732720+YongJuu@users.noreply.github.com>
2023-09-11 11:32:49 +0900

    [Bronze V] Title: 아스키 코드, Time: 196 ms, Memory: 17628 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Scanner kb = new Scanner(System.in);
+		
+		int a = kb.next().charAt(0); //얘는 아스키로 반환
+		System.out.println(a);
+		
+	}
+}
