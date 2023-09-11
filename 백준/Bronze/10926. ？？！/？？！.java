77b6bb0c639291756e18077e7873575e3045d4ad
YongJuu <39732720+YongJuu@users.noreply.github.com>
2023-09-11 11:28:54 +0900

    [Bronze V] Title: ??!, Time: 216 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+// 뭘 입력해도 이미 존재하는 아이디다. ?
+public class Main {	
+	//실행 메인 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Main T = new Main();
+		
+		Scanner kb = new Scanner(System.in);
+		//1) 입력 
+		String str = kb.next();
+		
+		//2) 출력 덧붙임
+		System.out.println(str+"??!");
+		
+	}
+}
