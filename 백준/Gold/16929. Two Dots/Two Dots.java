c19756c53e99bebb9273a5ee4341c35e5d535ece
YongJuu <39732720+YongJuu@users.noreply.github.com>
2023-11-08 15:59:52 +0900

    [Gold IV] Title: Two Dots, Time: 224 ms, Memory: 27040 KB -BaekjoonHub

---
+++ 백준/Gold/16929. Two Dots/Two Dots.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+/**
+ * 16929번. Two Dots - DFS 문풀 ㄴ
+ * @author MYLG
+ *
+ */
+public class Main {
+	static int N, M;
+	static char[][] map;
+	static boolean[][] visited;
+	//4방향
+	static int[] dx = {0, 0, 1, -1};
+	static int[] dy = {1, -1, 0, 0};
+	
+	static int firstX, firstY;
+	
+	//DFS -사이클 여부를 리턴
+	static boolean DFS(int x, int y, int cnt) {		
+		visited[x][y] = true;
+		
+		for(int k=0; k<4; k++) {
+			int nx = x + dx[k];
+			int ny = y + dy[k];
+			//경계 벗어나면 일단 안감 
+			if(nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+			
+			if(map[nx][ny] == map[x][y]) {
+				if(!visited[nx][ny]) {
+					visited[nx][ny] = true;
+					if(DFS(nx, ny, cnt+1)) return true;
+				}else {
+					if(cnt >= 4 && firstX == nx && firstY == ny) return true;
+				}
+			}
+			
+		
+		}
+		return false;
+	}
+	
+	//실행 메인 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner kb = new Scanner(System.in);
+		
+		N = kb.nextInt();
+		M = kb.nextInt();
+		map = new char[N][M];
+		visited = new boolean[N][M];
+		//초기화
+		for(int i=0; i<N; i++) {
+			String tmp = kb.next();
+			for(int j=0; j<M; j++) {
+				map[i][j] = tmp.charAt(j);
+			}
+		}
+	
+		boolean isCycle = false;
+		//호출
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<M; j++) {
+				visited= new boolean[N][M];
+				firstX = i;
+				firstY = j;
+				if(DFS(i, j, 1)) {
+					System.out.println("Yes");
+					return;
+				}
+			}
+		}
+		System.out.println("No");
+	}
+
+}
