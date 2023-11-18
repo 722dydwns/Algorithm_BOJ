446cdff603fa6a7bcea6d5293997deec56ebf249
YongJuu <39732720+YongJuu@users.noreply.github.com>
2023-11-18 15:26:26 +0900

    [Gold V] Title: 최소비용 구하기, Time: 1332 ms, Memory: 247140 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -3,75 +3,74 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
-/*백준 1916. 최소비용 구하기 */
-class Node implements Comparable<Node>{
-	int v;
-	int val;
-	Node(int v, int val){
-		this.v = v;
-		this.val = val;
+/**
+ * 1916번. 최소비용 구하기 = 다익스트라 문풀ㄴ
+ * @author MYLG
+ *
+ */
+class Edge implements Comparable<Edge>{
+	int e, val;
+	Edge(int e, int val){
+		this.e = e;
+		this.val =val;
 	}
 	@Override
-	public int compareTo(Node o) {
+	public int compareTo(Edge o) {
 		// TODO Auto-generated method stub
-		
-		return this.val - o.val; //오름차순 정렬 
+		return this.val - o.val;
 	}
 }
 public class Main {
+	static int N, M;
 	static int[] distance;
-	static boolean[] visited;
-	static ArrayList<ArrayList<Node>> graph;
+	static ArrayList<ArrayList<Edge>> graph;
+	static int S, E;
+	static PriorityQueue<int[]> pQ;
 	
 	//실행 메인 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner kb= new Scanner(System.in);
-		int N = kb.nextInt();
-		int E = kb.nextInt();
 		
+		N = kb.nextInt();
+		M = kb.nextInt();
 		distance= new int[N+1];
-		visited = new boolean[N+1];
-		graph = new ArrayList<>();
-		
-		for(int i=0; i<=N; i++) graph.add(new ArrayList<Node>());
-		
-		//distance 거리 배열 값 ㅈㄴ 큰 값으로 초기화
 		Arrays.fill(distance, Integer.MAX_VALUE);
 		
-		for(int i=0; i<E; i++) {
-			int st = kb.nextInt();
-			int ed = kb.nextInt();
-			int w = kb.nextInt();
-			graph.get(st).add(new Node(ed,w));
+		graph = new ArrayList<>();
+		for(int i=0; i<=N; i++) {
+			graph.add(new ArrayList<>());
 		}
 		
-		int S = kb.nextInt();
-		int Ed = kb.nextInt();
-		//----> 여기서부터 다익스트라 알고리즘 적용-기본 오름차순 
-		PriorityQueue<Node> Q = new PriorityQueue<>();
-		distance[S] = 0;
-		Q.add(new Node(S, 0));
+		//데이터 입력받기 방향 그래프임
+		for(int i=0; i<M; i++) {
+			int a = kb.nextInt();
+			int b = kb.nextInt();
+			int val = kb.nextInt();
+			graph.get(a).add(new Edge(b, val));
+		}
 		
-		while(!Q.isEmpty()) {
-			//현재 노드 정보 뽑기
-			Node cur = Q.poll();
-			int c_v = cur.v;
-			int c_val = cur.val;
-			if(!visited[c_v]) {//아직 방문 안한 정점에 대하여 
-				visited[c_v] = true;//방문처리 
-				for(Node nx : graph.get(c_v)) {
-					int nx_v = nx.v;
-					int nx_val = nx.val;
-					if(!visited[nx_v] && distance[nx_v] > distance[c_v]+nx_val) {
-						distance[nx_v] = distance[c_v] + nx_val;//다음 정점 거리 = 현재 정점 + 다음 가중치
-						Q.add(new Node(nx_v, distance[nx_v]));
-					}
+		S = kb.nextInt();
+		E = kb.nextInt();
+		//다익스트라 시작 
+		
+		pQ = new PriorityQueue<>((a, b)-> a[2]-b[2]);
+		
+		//시작점 처리
+		distance[S] = 0;
+		pQ.offer(new int[] {S, S, 0});//S에서 S 자신은 거리 0
+		
+		while(!pQ.isEmpty()) {
+			int[] cur = pQ.poll();
+			if(cur[2] > distance[cur[1]]) continue;
+			
+			for(Edge nx : graph.get(cur[1])) {
+				if(distance[nx.e] > distance[cur[1]] + nx.val) {
+					distance[nx.e] = distance[cur[1]] + nx.val;
+					pQ.offer(new int[] {cur[1], nx.e, distance[nx.e]});
 				}
 			}
 		}
-		//도착지 최단거리출력 
-		System.out.println(distance[Ed]);
-		
+		System.out.println(distance[E]);	
 	}
 }
