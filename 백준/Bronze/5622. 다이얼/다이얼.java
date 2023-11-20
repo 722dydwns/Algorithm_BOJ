36b58e023447c820b6e84d2a2bb6f3a8bd7aef1f
YongJuu <39732720+YongJuu@users.noreply.github.com>
2023-11-20 15:38:56 +0900

    [Bronze II] Title: 다이얼, Time: 204 ms, Memory: 17500 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.java
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -1,52 +1,53 @@
 import java.util.Scanner;
- 
+
+/**
+ * 5622번. 다이얼 - 구현 
+ * @author MYLG
+ *
+ */
 public class Main {
+	
+	//실행 메인 
 	public static void main(String[] args) {
-    
-		Scanner in = new Scanner(System.in);
+		// TODO Auto-generated method stub
+		Scanner kb= new Scanner(System.in);
+		String str = kb.next();
 		
-		String s = in.nextLine();
- 
-		int count = 0;
-		int k = s.length();
-        
-		for(int i = 0; i < k; i++) {
-        
-			switch(s.charAt(i)) {
-            
-			case 'A' : case 'B': case 'C' : 
-				count += 3;
-				break;
-                
-			case 'D' : case 'E': case 'F' :
-				count += 4; 
-				break;
-                
-			case 'G' : case 'H': case 'I' :
-				count += 5; 
-				break;
-                
-			case 'J' : case 'K': case 'L' : 
-				count += 6;
-				break;
-                
-			case 'M' : case 'N': case 'O' :
-				count += 7;
-				break;
-                
-			case 'P' : case 'Q': case 'R' : case 'S' : 
-				count += 8; 
-				break;
-                
-			case 'T' : case 'U': case 'V' : 
-				count += 9;
-				break;
-                
-			case 'W' : case 'X': case 'Y' : case 'Z' : 
-				count += 10;
-				break;
+		int time = 0;
+		for(int i=0; i<str.length(); i++) {
+			//총 글자수 만큼 반복할 건데 
+			char c = str.charAt(i);
+			
+			if(c == 'A' || c == 'B' || c == 'C') {
+				time += 3;
+				
 			}
-		}		
-		System.out.print(count);
+			if(c == 'D' || c == 'E' || c == 'F') {
+				time += 4;
+			}
+			if(c == 'G' || c == 'H' || c == 'I') {
+				time += 5;
+			}
+			if(c == 'J' || c == 'K' || c == 'L') {
+				time += 6;
+			}
+			if(c == 'M' || c == 'N' || c == 'O') {
+				time += 7;
+				
+			}
+			if(c == 'P' || c == 'Q' || c == 'R' || c == 'S') {
+				time += 8;
+			}
+			if(c == 'T' || c == 'U' || c == 'V') {
+				time += 9;
+				
+			}
+			if(c == 'W' || c == 'X' || c == 'Y' || c=='Z') {
+				time += 10;
+			}
+		}
+		
+		System.out.println(time);
 	}
+
 }
