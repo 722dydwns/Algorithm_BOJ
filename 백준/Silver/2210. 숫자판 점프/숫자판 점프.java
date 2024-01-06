24d8e9f4cdebac873f96fc5c38d34b7529b5f367
722dydwns <39732720+722dydwns@users.noreply.github.com>
2024-01-06 15:51:16 +0900

    [Silver II] Title: 숫자판 점프, Time: 224 ms, Memory: 18772 KB -BaekjoonHub

--- 백준/Silver/2210. 숫자판 점프/숫자판 점프.java
+++ 백준/Silver/2210. 숫자판 점프/숫자판 점프.java
@@ -1,28 +1,30 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
-/*2210번. 숫자판 점프 - DFS*/
+/**
+ * 2210번. 숫자판 점프 
+ * @author MYLG
+ *
+ */
 public class Main {
-	static String [][] board;
-	static HashMap<String, Integer> map = new HashMap<>();
-	//4방향 
+	static int[][] arr;
 	static int[] dx = {0, 0, 1, -1};
 	static int[] dy = {1, -1, 0, 0};
-	
+	static Map<Integer, Integer> map = new HashMap<>();
 	//DFS
-	static void DFS(int x, int y, int lv, String num) {
+	static void DFS(int lv, int x, int y, int num) {
 		if(lv == 5) {
-			map.put(num, map.getOrDefault(num, 0)+1);//카운팅 하면서 
-			return;//여기서 복귀함
+			map.put(num, map.getOrDefault(num, 0) + 1);
+			return;
 		}
 		
 		for(int k=0; k<4; k++) {
 			int nx = x + dx[k];
 			int ny = y + dy[k];
-			if(nx <0 || ny<0 || nx >=5 || ny>=5 ) continue;
+			if(nx < 0 || nx >= 5 || ny <0 || ny >= 5) continue;
 			
-			DFS(nx, ny, lv+1, num + board[nx][ny]);//더 깊이 감
-		
+			DFS(lv+1, nx, ny, (num *10) + arr[nx][ny]);
 		}
 	}
 	
@@ -31,21 +33,19 @@ public class Main {
 		// TODO Auto-generated method stub
 		Scanner kb= new Scanner(System.in);
 		
-		board = new String[5][5];
+		arr = new int[5][5];
 		
 		for(int i=0; i<5; i++) {
 			for(int j=0; j<5; j++) {
-				board[i][j] = kb.next();
+				arr[i][j] = kb.nextInt();
 			}
 		}
-		//하나씩 보냄
+		
 		for(int i=0; i<5; i++) {
 			for(int j=0; j<5; j++) {
-				DFS(i, j, 0, board[i][j]);
+				DFS(0, i, j, arr[i][j]);
 			}
 		}
 		System.out.println(map.size());
-		
 	}
-
 }
