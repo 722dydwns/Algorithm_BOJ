a5fa9c43867ba958dd4179d095eb5e1f37dd5089
722dydwns <39732720+722dydwns@users.noreply.github.com>
2024-01-08 10:01:03 +0900

    [Silver III] Title: 계단 오르기, Time: 224 ms, Memory: 17876 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,24 +1,35 @@
+
 import java.util.Scanner;
 
-/*2579번. 계단 오르기 */
+/**
+ * 백준 2579번. 계단 오르기 - DP 문풀 
+ * @author MYLG
+ *
+ */
 public class Main {
-
+	
+	//실행 메인 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner kb= new Scanner(System.in);
-		int N = kb.nextInt();//계단 개수 
-		int[] score = new int[N+1];
-		for(int i=1; i<=N; i++) score[i] = kb.nextInt();
+		int N = kb.nextInt();
 		
-		int[] D = new int[N+1];
-		D[1] = score[1];
-		if(N > 1) D[2] = score[1] + score[2];
+		int[] arr = new int[N+1];
+		int[] dy = new int[N+1];
+		
+		for(int i=1; i<=N; i++) {
+			arr[i]= kb.nextInt();
+		}
+		
+		//[주의] : 계단의 개수는 300개 이하 자연수(1~300) 이니까 극단적으로 1이 들어오는 것도 가능함
+		dy[1] = arr[1];
+		if(N  >= 2) dy[2] = arr[1] + arr[2];
 		
 		for(int i=3; i<=N; i++) {
-			D[i] = Math.max(D[i-2] + score[i], D[i-3] + score[i-1]+ score[i]);
+			//i번째 칸 = 1) 직전 칸(2칸+1칸) + 현재칸       2) 더플 직전칸 (2칸 전) + 현재 칸 
+			dy[i] = Math.max(dy[i-3]+arr[i-1] +arr[i], dy[i-2] + arr[i]);
 		}
-		System.out.println(D[N]);
 		
-	}
-
+		System.out.println(dy[N]);
+	}	
 }
