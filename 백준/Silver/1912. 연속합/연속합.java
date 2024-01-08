8e499f73929527d2146e292c8bbc47b70b733e0a
722dydwns <39732720+722dydwns@users.noreply.github.com>
2024-01-08 10:25:19 +0900

    [Silver II] Title: 연속합, Time: 788 ms, Memory: 102068 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -1,26 +1,34 @@
 import java.util.Scanner;
 
-//1912번. 연속합 
+/**
+ * 1912번. 연속합 - DP 문풀 
+ * @author MYLG
+ *
+ */
 public class Main {
+	
 	//실행 메인 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner kb = new Scanner(System.in);
-		int n = kb.nextInt();
-		int[] arr =new int[n];
+		int N = kb.nextInt();
+		int[] arr = new int[N+1];
+		int[] dy =  new int[N+1];
 		
-		for(int i=0; i<n; i++) arr[i] = kb.nextInt();
+		for(int i=1; i<=N; i++) arr[i] = kb.nextInt();
 		
-		int[] dy = new int[n];//i를 마지막 선택 항으로 하는 최대합
-		
-		dy[0] = arr[0];//초기화
-		int answer = dy[0];//얘로 초기화 해두고 
-		for(int i=1; i<n; i++) {
-			//직전까지의 합에 연달아 합치기 vs 지금부터 시작하기 중에 더 큰 값으로 세팅 
-			dy[i] = Math.max(arr[i], dy[i-1] + arr[i]);
-			answer = Math.max(answer, dy[i]);
+		dy[1] = Math.max(arr[1], dy[0] + arr[1]);
+		for(int i=2; i<=N; i++) {
+			//직전합에 현재값 더한 것(=N을 마지막으로 하는 합) vs N부터 시작하는 합 중 큰 값 세팅 
+			dy[i] = Math.max(dy[i-1] + arr[i], arr[i]);
 		}
-		System.out.println(answer);
+		int max = Integer.MIN_VALUE;
+		//dy[]에 담긴 값들 중 최대값
+		for(int i=1; i<=N; i++) {
+			max = Math.max(max, dy[i]);
+		}
 		
+		System.out.println(max);
 	}
+
 }
