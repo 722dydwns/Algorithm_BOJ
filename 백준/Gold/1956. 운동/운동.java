de92a1acc72c945df39f1e639f10cf7c215d6e16
722dydwns <39732720+722dydwns@users.noreply.github.com>
2024-01-17 09:46:10 +0900

    [Gold IV] Title: 운동, Time: 1540 ms, Memory: 309560 KB -BaekjoonHub

--- 백준/Gold/1956. 운동/운동.java
+++ 백준/Gold/1956. 운동/운동.java
@@ -1,60 +1,61 @@
 import java.util.Scanner;
 
-/*1956번. 운동 - 플로이드 문풀 */
+/**
+ * 1956번. 운동 - 플로이드 문풀 
+ * @author MYLG
+ *
+ */
 public class Main {
-	static int V, E;
-	static int[][] distance;
-	static final int INF = 987654321;
-	
+	static final int INF = 10000001;
 	//실행 메인 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner kb= new Scanner(System.in);
-		V = kb.nextInt();
-		E = kb.nextInt();
+		int V = kb.nextInt();
+		int E = kb.nextInt();
 		
-		distance =new int[V+1][V+1];
-		//인접 행렬 초기화 
+		int[][] distance = new int[V+1][V+1];
+		
 		for(int i=1; i<=V; i++) {
 			for(int j=1; j<=V; j++) {
 				if(i==j) distance[i][j] = 0;
-				else distance[i][j] = INF;
+				else {
+					distance[i][j] = INF;
+				}
 			}
 		}
 		
-		//간선 개수 만큼 입력받기 
+		//세팅
 		for(int i=0; i<E; i++) {
-			int a= kb.nextInt();
+			int a = kb.nextInt();
 			int b = kb.nextInt();
-			int c = kb.nextInt();
-			//단방향 
-			distance[a][b] = c;
+			int val = kb.nextInt();
+			if(distance[a][b] > val) {
+				distance[a][b] = val;
+			}
 		}
 		
-		//플로이드 시작 
-		for(int k=1; k<=V; k++) { //걍유지 K 
+		//플로이드 시작
+		for(int k = 1; k<=V; k++) {
 			for(int i=1; i<=V; i++) {
 				for(int j=1; j<=V; j++) {
-					if(i==j) continue;
 					if(distance[i][j] > distance[i][k] + distance[k][j]) {
-						distance[i][j] = distance[i][k] + distance[k][j];//갱신 
+						distance[i][j] = distance[i][k] + distance[k][j];
 					}
 				}
 			}
 		}
-	
-		int ans = INF;
+		
+		int answer = INF;
 		for(int i=1; i<=V; i++) {
 			for(int j=1; j<=V; j++) {
-				if(i==j) continue;
-				//서로 INF가 아닌 값들이 사이클 형성 된 상태인 것이다. 
-				if(distance[i][j] != INF && distance[j][i] != INF) {
-					ans = Math.min(ans, distance[i][j] + distance[j][i]);
+				if(i != j && distance[i][j] != INF && distance[j][i] != INF) {
+					answer = Math.min(answer, distance[i][j] + distance[j][i]);
 				}
 			}
 		}
 		
-		if(ans == INF) System.out.println(-1);
-		else System.out.println(ans);
+		if(answer == INF) System.out.println(-1);
+		else System.out.println(answer);
 	}
 }
