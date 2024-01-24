ae764fabf52c99585b84d120467054a78fde54f8
722dydwns <39732720+722dydwns@users.noreply.github.com>
2024-01-24 14:57:47 +0900

    [Platinum V] Title: 행성 터널, Time: 2244 ms, Memory: 263208 KB -BaekjoonHub

--- 백준/Platinum/2887. 행성 터널/행성 터널.java
+++ 백준/Platinum/2887. 행성 터널/행성 터널.java
@@ -1,13 +1,15 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
 /**
- * 2887번. 행성 터널 - 최소비용신장트리 문풀 
+ * 2887번. 행성 터널 - MST 문풀 
  * @author MYLG
  *
  */
-class Planet{
+class Planet {
 	int num;
 	int x, y, z;
 	Planet(int num, int x, int y, int z){
@@ -17,34 +19,33 @@ class Planet{
 		this.z = z;
 	}
 }
-
 class Edge implements Comparable<Edge>{
-	int st, ed, val;
-	Edge(int st, int ed, int val){
-		this.st = st;
-		this.ed = ed;
+	int s, e, val;
+	Edge(int s, int e, int val){
+		this.s= s;
+		this.e = e;
 		this.val = val;
 	}
 	@Override
 	public int compareTo(Edge o) {
 		// TODO Auto-generated method stub
-		return this.val - o.val;//가중치 적은 거 우선임
+		return this.val - o.val;
 	}
 }
 public class Main {
-	static int N;
 	static int[] parent;
 	//find
 	static int find(int a) {
-		if(a == parent[a]) return a;
-		else return parent[a] = find(parent[a]);
+		if(a==parent[a]) return a;
+		return parent[a] =find(parent[a]);
 	}
+	
 	//union
 	static void union(int a, int b) {
 		a = find(a);
 		b = find(b);
 		if(a!=b) {
-			parent[b] = a;
+			parent[b]= a;
 		}
 	}
 	
@@ -52,50 +53,50 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner kb= new Scanner(System.in);
-		N = kb.nextInt();
-		parent = new int[N+1];
-		for(int i=1; i<=N; i++) parent[i] = i;//자기 자신으로 우선 초기화
+		int N = kb.nextInt();
 		
-		Planet[] pList = new Planet[N];
+		parent = new int[N+1];
+		for(int i=1; i<=N; i++) parent[i] =i;
+		
+		List<Planet> pList = new ArrayList<>();
 		for(int i=0; i<N; i++) {
-			int x = kb.nextInt();
+			int x= kb.nextInt();
 			int y = kb.nextInt();
-			int z = kb.nextInt();
-			pList[i] = new Planet(i, x, y, z);
+			int z =kb.nextInt();
+			pList.add(new Planet(i+1, x, y, z))	;
 		}
 		
 		PriorityQueue<Edge> pQ = new PriorityQueue<>();
-		//x좌표 기준으로 정렬 
-		Arrays.sort(pList, (p1, p2) -> Integer.compare(p1.x, p2.x));
+		
+		Collections.sort(pList, (p1, p2) -> Integer.compare(p1.x, p2.x));
 		for(int i=1; i<N; i++) {
-			int val = Math.abs(pList[i].x - pList[i-1].x);
-			pQ.offer(new Edge(pList[i].num, pList[i-1].num, val));
+			int val = Math.abs(pList.get(i).x - pList.get(i-1).x);
+			pQ.offer(new Edge(pList.get(i).num, pList.get(i-1).num, val));
 		}
 		
-		//y좌표 기준으로 정렬 
-		Arrays.sort(pList, (p1, p2) -> Integer.compare(p1.y, p2.y));
+		Collections.sort(pList, (p1, p2) -> Integer.compare(p1.y, p2.y));
 		for(int i=1; i<N; i++) {
-			int val = Math.abs(pList[i].y - pList[i-1].y);
-			pQ.offer(new Edge(pList[i].num, pList[i-1].num, val));
-		}
-		//z좌표 기준으로 정렬 
-		Arrays.sort(pList, (p1, p2) -> Integer.compare(p1.z, p2.z));
-		for(int i=1; i<N; i++) {
-			int val = Math.abs(pList[i].z - pList[i-1].z);
-			pQ.offer(new Edge(pList[i].num, pList[i-1].num, val));
+			int val = Math.abs(pList.get(i).y - pList.get(i-1).y);
+			pQ.offer(new Edge(pList.get(i).num, pList.get(i-1).num, val));
 		}
 		
-		int useEdge = 0, useCost=0;
-		//mst
-		while(useEdge != N-1) { //간선 N-1개 연결함
-			Edge cur = pQ.poll();
-			if(find(cur.st) != find(cur.ed)) {
-				union(cur.st , cur.ed);
-				useCost += cur.val;//가중치 합
-				useEdge++;//간선 ++ 처리
+		Collections.sort(pList, (p1, p2) -> Integer.compare(p1.z, p2.z));
+		for(int i=1; i<N; i++) {
+			int val = Math.abs(pList.get(i).z - pList.get(i-1).z);
+			pQ.offer(new Edge(pList.get(i).num, pList.get(i-1).num, val));
+		}
+		
+		int useEdge= 0;
+		int useCost = 0;
+		
+		while(useEdge < N-1) {
+			Edge edge = pQ.poll();
+			if(find(edge.s) != find(edge.e)) {
+				union(edge.s, edge.e);
+				useCost += edge.val;
+				useEdge++;
 			}
 		}
-		
-		System.out.println(useCost);//최소비용	
+		System.out.println(useCost);
 	}
 }
