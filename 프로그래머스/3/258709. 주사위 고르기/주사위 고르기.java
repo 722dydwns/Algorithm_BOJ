3a8266326ebfcf1807db4026eadbb8825ed242db
722dydwns <39732720+722dydwns@users.noreply.github.com>
2024-02-01 18:01:44 +0900

    [level 3] Title: 주사위 고르기, Time: 878.38 ms, Memory: 155 MB -BaekjoonHub

--- 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.java
+++ 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.java
@@ -1,113 +1,104 @@
 import java.util.*;
 
 class Solution {
-    static int N;
+    static int diceCount;
     static boolean[] visited;
-    static List<int[]> diceA = new ArrayList<>();
-    static List<Integer> sumA;
-    static List<Integer> sumB;
-    //이분 탐색 
-    static int binarySearch(Integer a){
-        int left = 0;
-        int right = sumB.size();
-        
-        while(left + 1 < right){
-            int mid = (left + right) / 2;
-            if(a > sumB.get(mid)){
-                left = mid;
-            }else right = mid;
-        }
-        return left;
-    }
+    static List<int[]> A = new ArrayList<>();
+    static List<int[]> B = new ArrayList<>();
     
-    //경우의 수 
-    static void combSum(int lv, int[] d, int[][] dice, int sum, List<Integer> sumList){
-        //경우의 수 끝
-        if(lv == N/2) {
-            //다 고름
-            sumList.add(sum);
-            return;
-        }
-        //구성 별로 다 돌기
-        for(int i=0; i<6; i++){
-            combSum(lv+1, d, dice, sum + dice[d[lv]][i], sumList);
-        }
-    }
-    //selectB
-    static void selectB(int[] dA, int[] dB, boolean[] selected){
-        //dA 값은 해당 번째 주사위를 A가 갖고 갔다 의미
-        //이미 dA에 속한 값은 true로 만들고, 다음에 false인 값들을 dB에 추가
-        for(int select : dA){
-            selected[select] = true;
-        }
-        //B가 갖는 주사위 반영
-        int idx = 0;
-        for(int i=0; i<N; i++){
-            if(!selected[i]) dB[idx++] = i;
-        }
-    }
+    static int[] result;
+    static List<Integer> A_result;
+    static List<Integer> B_result;
     
-    //A는 주사위를 선택하는 경우의 수들을 diceA에 넣음
-    //B는 이거 제외 주사위임
-    static void selectA(int lv, int now, int[] arr){
-        if(lv == N/2){
-            diceA.add(arr.clone());
-            return;
-        }
-        
-        for(int i= now; i<N; i++){
-            if(!visited[i]){
-                visited[i] = true;
-                arr[lv] = i;
-                selectA(lv+1, i+1, arr);
-                visited[i] = false;
+    // 재귀로 주사위 combination
+    static void combination(int idx, int count){
+        if(count == diceCount/2){
+            int[] Atmp = new int[diceCount/2];
+            int[] Btmp = new int[diceCount/2];
+            
+            int A_idx =0;
+            int B_idx =0;
+            for(int i=0; i<diceCount; i++){
+                if(visited[i]){
+                    Atmp[A_idx] = i+1;
+                    A_idx++;
+                }
+                if(!visited[i]){
+                    Btmp[B_idx] = i+1;
+                    B_idx++;
+                }
             }
+            //각각의 조합 구성을 각 리스트에 담는다. 
+            A.add(Atmp); 
+            B.add(Btmp);
+            return;
+        }
+        
+        //전체 주사위 순회하면서
+        for(int i=idx; i<diceCount; i++){
+            visited[i] = true;
+            combination(i+1, count+1);
+            visited[i] = false;
         }
     }
     
-    //솔루션 함수
+    //calculate -> 각 눈의 합 종류 담음
+    static void calculate(int idx, int[] dices, int[][] originDices, int sum, List<Integer> team){
+        if(idx == dices.length){
+            team.add(sum);
+            return;
+        }
+        for(int i=0; i<6; i++){
+            calculate(idx+1, dices, originDices, sum + originDices[dices[idx]-1][i], team);
+        }
+    }
+    
+    //솔루션 함수 
     public int[] solution(int[][] dice) {
-        int[] answer = {};
-        N = dice.length;
+        diceCount = dice.length;
+        visited = new boolean[diceCount];
         
-        //A 주사위 고르자
-        visited= new boolean[N];
-        selectA(0, 0, new int[N/2]);
+        //A,B에게 주사위 절반씩 나눌 수 있는 구성 전체 경우의 수 구함
+        combination(0, 0);        
         
         int max = Integer.MIN_VALUE;
-        for(int[] dA : diceA){
-            int[] dB = new int[N/2];
-            boolean[] selected = new boolean[N];
-            
-            //B 주사위도 골므
-            selectB(dA, dB, selected);
-            
-            //이제 비교
-            //각 A, B 주사위 경우의 수 합, 비교
-            sumA = new ArrayList<>();
-            sumB = new ArrayList<>();
-            combSum(0, dA, dice, 0, sumA);
-            combSum(0, dB, dice, 0, sumB);
-            
-            //비교
-            Collections.sort(sumA);
-            Collections.sort(sumB);
-            
-            //sumA의 각 원소들이 sumB의 어디까지 이길 수 있는지 알면
-            int count = 0;
-            for(Integer a : sumA){
-                count += binarySearch(a);
-            }
-            //이번에 구한 거랑 지금까지랑 비교
-            if(count > max){
-                max = count;
-                answer = dA;
-            }
-        }
+        int index = 0;
         
-        for(int i=0; i<N/2; i++){
-            answer[i]++;
+        for(int i=0; i<A.size(); i++){
+            A_result = new ArrayList<>();
+            B_result = new ArrayList<>();
+            //A,B 각각 조합에 대한 주사위 눈 합
+            calculate(0, A.get(i), dice, 0, A_result);
+            calculate(0, B.get(i), dice, 0, B_result);
+            //A,B 각각 조합에 대한 눈의 합 구한 리스트 오름차순 정렬 
+            Collections.sort(A_result);
+            Collections.sort(B_result);
+            
+            //A가 이길 경우를 구해야 함 
+            int totalWinCount=0; 
+            for(int a : A_result) { 
+                //A 주사위의 조합으로 나올 수 있는 모든 합 각각을 돌면서
+                int left = 0;
+                int right = B_result.size();
+                
+                while(left+1 < right){
+                    int mid = (left+right)/2; 
+                    //A의 현재 합이 B의 중앙위치의 합보다 큰지 작은지에 따른 이분탐색 
+                    if(a > B_result.get(mid)){
+                        left=mid; //크다면 뒤쪽 범위에서 다시 탐색
+                    }else right = mid;//작다면 앞쪽 범위에서 탐색 
+                }
+                //이기는 경우의 합을 다 더함
+                totalWinCount += left;
+            }
+            //가장 큰 승률로 갱신
+            if(max < totalWinCount){
+                max = totalWinCount;
+                index = i;
+            }
         }
-        return answer;
+        result = A.get(index);
+        
+        return result;
     }
 }
