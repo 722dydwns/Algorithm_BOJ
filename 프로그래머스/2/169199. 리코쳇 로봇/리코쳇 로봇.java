610dae59d0e316918f83a2d0d1095086494d6dc3
722dydwns <39732720+722dydwns@users.noreply.github.com>
2024-02-07 13:54:12 +0900

    [level 2] Title: 리코쳇 로봇, Time: 1.65 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -0,0 +1,78 @@
+import java.util.*;
+class Node{
+    int x, y, count;
+    Node(int x, int y, int count){
+        this.x = x;
+        this.y = y;
+        this.count = count;
+    }
+}
+class Solution{
+    //4방향
+    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {1, -1, 0, 0};
+    static char[][] map;
+    
+    //솔루션 함수 
+    public int solution(String[] board) {
+        int answer = Integer.MAX_VALUE;
+        int N = board.length;
+        int M = board[0].length();
+        map = new char[N][M];
+        
+        int[] st = new int[2];
+        
+        for(int i=0; i<N; i++){
+            for(int j=0; j<M; j++){
+                map[i][j] = board[i].charAt(j);
+                if(map[i][j] == 'R'){ // 시작점 세팅 
+                    st[0] = i;
+                    st[1] = j;
+                }
+            }
+        }
+        
+        boolean[][] visited = new boolean[N][M];
+        visited[st[0]][st[1]] = true;
+        
+        Queue<Node> Q = new LinkedList<>();
+        Q.offer(new Node(st[0], st[1], 0));
+        
+        while(!Q.isEmpty()){
+            Node cur = Q.poll();
+            
+            if(answer <= cur.count){
+                continue;
+            }
+            if(map[cur.x][cur.y] == 'G'){
+                answer = Math.min(answer, cur.count);
+                continue;
+            }
+            
+            for(int k=0; k<4; k++){
+                int nx = cur.x + dx[k];
+                int ny = cur.y + dy[k];
+                
+                if(nx <0 || ny <0 || nx >= N || ny >= M || map[nx][ny] == 'D') continue;
+                
+                while(nx >=0 && ny >= 0 && nx < N && ny <M && map[nx][ny] != 'D') {
+                    nx += dx[k];
+                    ny += dy[k];
+                }
+                
+                nx -= dx[k];
+                ny -= dy[k];
+                
+                if(visited[nx][ny]) continue;
+                
+                visited[nx][ny] = true;
+                Q.offer(new Node(nx, ny, cur.count + 1));
+                
+            }
+            
+        }
+        
+        
+        return answer == Integer.MAX_VALUE ? -1 : answer;
+    }
+}
