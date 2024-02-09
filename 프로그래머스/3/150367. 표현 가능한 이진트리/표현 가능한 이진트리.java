ff0bfb3b347cc9033906ca5cb2d42d642ce08204
722dydwns <39732720+722dydwns@users.noreply.github.com>
2024-02-09 19:07:07 +0900

    [level 3] Title: 표현 가능한 이진트리, Time: 193.93 ms, Memory: 219 MB -BaekjoonHub

--- 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
+++ 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
@@ -1,42 +1,71 @@
 import java.util.*;
-public class Solution {
-	private static int[] answer;
-	private static boolean dfs(String number) {
-		boolean valid = true;
-		
-		int mid = (number.length()-1)/2;
-		char root = number.charAt(mid);
-		String left = number.substring(0,mid);
-		String right = number.substring(mid+1,number.length());
-		
-		if(root == '0' && (left.charAt((left.length()-1)/2)=='1' || right.charAt((right.length()-1)/2)=='1')){
-			return false;
-		}
-		
-		if(left.length() >= 3) {
-			valid = dfs(left);
-			if(valid) {
-				valid = dfs(right);
-			}
-		}
-		return valid;
-	}
-    public int[] solution(long[] numbers) {
-        answer = new int [numbers.length];
-        
-        for(int i = 0; i < numbers.length; i++) {
-        	String cur = Long.toBinaryString(numbers[i]);
-        	int j = 0;
-        	while((int)Math.pow(2, j)-1 < cur.length()) {
-        		j++;
-        	}
-        	while((int)Math.pow(2, j)-1 != cur.length()) {
-        		cur = "0"+ cur;
-        	}
-        	if(dfs(cur)) {
-        		answer[i] = 1;
-        	}
+
+class Solution {
+
+    private static final int MAX_H = 6;
+
+    public List<Integer> solution(long[] numbers) {
+        List<Integer> answer = new ArrayList<>();
+
+        for (long number : numbers) {
+            answer.add(solution(number));
         }
+
         return answer;
+    }
+
+    private int solution(long number) {
+        //1) 이진수로 변환
+        String binaryString = Long.toBinaryString(number);
+
+        // 왼쪽에 더할 수 있는 0 개수
+        int canAddZeroCount = binaryString.length() - 1;
+        for (int addZeroCount = 0; addZeroCount <= canAddZeroCount; addZeroCount++) {
+            String zeros = "";
+            for (int i = 0; i < addZeroCount; i++) {
+                zeros += "0";
+            }
+
+            String curBinaryString = zeros + binaryString;
+            if (isPerfectBinaryTreeNodeCount(curBinaryString.length())) {
+                if (canMakeTree(curBinaryString)) {
+                    return 1;
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    private boolean isPerfectBinaryTreeNodeCount(int length) {
+        for (int h = 1; h <= MAX_H; h++) {
+            if (length == (int)Math.pow(2, h) - 1) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean canMakeTree(String tree) {
+        // 항상 tree.length() 는 홀수 이다.
+
+        if (tree.length() == 1) {
+            return true;
+        }
+
+        int midIndex = tree.length() / 2;
+
+        if (tree.charAt(midIndex) == '1') {
+            // 일단 현재 상태에서 루트가 1 인걸 확인 -> 좌우 자식트리 확인
+            return canMakeTree(tree.substring(0, midIndex)) && canMakeTree(tree.substring(midIndex + 1, tree.length()));
+        } else {
+            // 현재 루트가 0 -> 자식 노드들이 전부 더미노드 즉 0이여야 함
+            if (tree.contains("1")) {
+                return false;
+            } else {
+                return true;
+            }
+        }
     }
 }
