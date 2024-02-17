eecfa550d833fa04dbeb8aee852120df37074f9b
722dydwns <39732720+722dydwns@users.noreply.github.com>
2024-02-17 13:00:49 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.16 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -4,52 +4,31 @@ class Solution {
     private static char[][] map;
     //솔루션 함수 
     public int[] solution(String[] wallpaper) {
-        int[] answer = new int[4];
+        int[] answer;
         
         int N = wallpaper.length;
         int M = wallpaper[0].length();
         
         map = new char[N][M];
         
-        PriorityQueue<int[]> xQ = new PriorityQueue<> ((a, b) -> a[0] - b[0]); //x좌표 최소 우선순위
-        PriorityQueue<int[]> yQ = new PriorityQueue<> ((a, b) -> a[1] - b[1]); //y좌표 최소 우선순위 
-        PriorityQueue<int[]> xMaxQ = new PriorityQueue<>((a, b) -> b[0] - a[0]);
-        PriorityQueue<int[]> yMaxQ = new PriorityQueue<>((a, b) -> b[1] - a[1]);
+        int minX = Integer.MAX_VALUE;
+        int minY = Integer.MAX_VALUE;
+        
+        int maxX = Integer.MIN_VALUE;
+        int maxY = Integer.MIN_VALUE;
         
         for(int i=0; i<N; i++){
             for(int j=0; j<M; j++){
                 map[i][j] = wallpaper[i].charAt(j);
                 if(map[i][j] == '#'){
-                    xQ.offer(new int[] {i, j});
-                    yQ.offer(new int[] {i, j}); 
-                    xMaxQ.offer(new int[] {i, j});
-                    yMaxQ.offer(new int[] {i, j});
+                    minX = Math.min(minX, i);
+                    minY = Math.min(minY, j);
+                    maxX = Math.max(maxX, i+1);
+                    maxY = Math.max(maxY, j+1);
                 }
             }
         }
-        
-        if(xQ.size() == 1){
-            int[] tmp = xQ.poll();
-            answer[0] = tmp[0];
-            answer[1] = tmp[1];
-            answer[2] = tmp[0] + 1;
-            answer[3] = tmp[1] + 1;
-            
-            return answer;
-        }
-        
-        int[] minX = xQ.poll();
-        int[] minY = yQ.poll();
-        
-        answer[0] = minX[0];
-        answer[1] = minY[1];
-        
-        int[] maxX = xMaxQ.poll();
-        int[] maxY = yMaxQ.poll();
-        
-        answer[2] = maxX[0] +  1;
-        answer[3] = maxY[1] + 1;
-        
-        return answer;
+    
+        return new int[] {minX, minY, maxX, maxY};
     }
 }
