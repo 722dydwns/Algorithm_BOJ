391780a4b293b6718f63231340eac426a0badfb2
722dydwns <39732720+722dydwns@users.noreply.github.com>
2024-02-17 13:41:09 +0900

    [level 2] Title: 문자열 압축, Time: 1.38 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -8,11 +8,9 @@ class Solution {
         for (int length = 1; length <= s.length(); length++) {
             List<String> tokens = getSplitTokens(s, length);
 
-            StringBuilder compressedString = getCompressedString(tokens);
-
-            if (compressedString.length() < answer) {
-                answer = compressedString.length();
-            }
+            int compLen = getCompressedString(tokens);
+            
+            answer = Math.min(answer, compLen);
         }
 
         return answer;
@@ -20,7 +18,7 @@ class Solution {
 
     private List<String> getSplitTokens(String s, int length) {
         List<String> tokens = new ArrayList<>();
-        for (int startIndex = 0; startIndex < s.length(); startIndex += length) {
+        for (int startIndex = 0; startIndex < s.length(); startIndex += length){
             int endIndex = startIndex + length;
             if (endIndex > s.length()) {
                 endIndex = s.length();
@@ -29,25 +27,25 @@ class Solution {
         }
         return tokens;
     }
-
-    private StringBuilder getCompressedString(List<String> tokens) {
-        StringBuilder builder = new StringBuilder();
-        String last = "";
-        int count = 0;
-        for (String token : tokens) {
-            if (token.equals(last)) {
-                // 이전 토큰과 현재 토큰이 같을 때
-                count++;
-            } else {
-                // 새로운 토큰이 등장한 경우
-                if (count > 1) builder.append(count);
-                builder.append(last);
-                last = token;
-                count = 1;
+    private int getCompressedString(List<String> list){
+        String answer="";
+        String tmp= list.get(0);
+        int cnt =1;
+        for(int i=1; i<list.size(); i++){
+            if(tmp.equals(list.get(i))){
+                cnt++;
+            }else{
+                if(cnt>1) answer += String.valueOf(cnt);
+                cnt = 1;
+                answer += tmp;
+                tmp = list.get(i);
             }
         }
-        if (count > 1) builder.append(count);
-        builder.append(last);
-        return builder;
+        //내가 다른 글자 나타날 때마다 answer에 누적해서 String을 합쳐주고 있기 떄문에
+        //필연적으로 마지막 문자 종류는 (더 이상 자기와 다른 다음 문자가 없으므로) 붙이지 못하고 반복문을 탈출한다.
+        //따라서 마지막 문자는 반복문에 나와서 처리해줄 필요가 있다.
+        if(cnt > 1) answer += String.valueOf(cnt);
+        answer += tmp;
+        return answer.length();
     }
 }
