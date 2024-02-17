f405c02565553e25ba7bfa8aa4b962fd766703e8
722dydwns <39732720+722dydwns@users.noreply.github.com>
2024-02-17 18:16:33 +0900

    [level 3] Title: 합승 택시 요금, Time: 15.62 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -1,74 +1,40 @@
 import java.util.*;
 
-class Edge implements Comparable<Edge>{
-    int index;
-    int cost;
-    Edge(int index, int cost){
-        this.index = index;
-        this.cost = cost;
-    }
-    @Override
-    public int compareTo(Edge e){
-        return this.cost - e.cost;
-    }
-}
-
 class Solution {
-
-    static final int MAX = 20000001; // 200 * 100000 + 1
-    static ArrayList<ArrayList<Edge>> graph;
-
+    private static int[][] map;
+    
+    //솔루션 함수 
     public int solution(int n, int s, int a, int b, int[][] fares) {
-        int answer = Integer.MAX_VALUE;
-
-        graph = new ArrayList<>();
-        for(int i = 0 ; i <= n ; i ++){
-            graph.add(new ArrayList<>());
+        
+        map = new int[n+1][n+1];
+        for(int i=1; i<=n; i++){
+            Arrays.fill(map[i], 1000001);
+            map[i][i] = 0;
         }
 
-        for(int[] i : fares){
-            graph.get(i[0]).add(new Edge(i[1], i[2]));
-            graph.get(i[1]).add(new Edge(i[0], i[2]));
+        for(int[] x : fares){
+            int c = x[0];
+            int d = x[1];
+            map[c][d] = x[2];
+            map[d][c] = x[2];
         }
-
-        int[] startA = new int[n + 1];
-        int[] startB = new int[n + 1];
-        int[] start = new int[n + 1];
-
-        Arrays.fill(startA, MAX);
-        Arrays.fill(startB, MAX);
-        Arrays.fill(start, MAX);
-
-        startA = dijkstra(a, startA);
-        startB = dijkstra(b, startB);
-        start = dijkstra(s, start);
-
-        for(int i = 1; i <= n ; i ++) answer = Math.min(answer, startA[i] + startB[i] + start[i]);
-        return answer;
-    }
-
-    static int[] dijkstra (int start, int[] costs) {
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        pq.offer(new Edge(start, 0));
-        costs[start] = 0;
-
-        while (!pq.isEmpty()) {
-            Edge now = pq.poll();
-            int nIndex = now.index;
-            int nCost = now.cost;
-
-            if(nCost > costs[nIndex]) continue;
-
-            ArrayList<Edge> edges = graph.get(nIndex);
-            for (Edge edge : edges) {
-                int cost = costs[nIndex] + edge.cost;
-
-                if (cost < costs[edge.index]) {
-                    costs[edge.index] = cost;
-                    pq.offer(new Edge(edge.index, cost));
+        
+        //플로이드
+        for(int k=1; k<=n; k++){
+            for(int i=1; i<=n; i++){
+                for(int j=1; j<=n; j++){
+                    map[i][j] = Math.min(map[i][j], map[i][k] + map[k][j]);
                 }
             }
         }
-        return costs;
+    
+        
+        int answer = map[s][a] + map[s][b];
+        //경유지점을 다른 지점 하나씩 해보기 
+        for(int k=1; k<=n; k++){
+            answer = Math.min(answer, map[s][k] + map[k][a] + map[k][b]);
+        }
+        
+        return answer;
     }
 }
