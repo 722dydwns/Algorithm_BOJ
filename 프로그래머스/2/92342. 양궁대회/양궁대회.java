7c1dfbd3a36979f03fc1ebce929ac2e3cbd24fe9
722dydwns <39732720+722dydwns@users.noreply.github.com>
2024-02-27 19:32:59 +0900

    [level 2] Title: 양궁대회, Time: 4.43 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -1,40 +1,52 @@
 import java.util.*;
 
 class Solution {
-    static int[] res = { -1 };
-    static int[] lion;
-    static int max = -1000;
-    public void dfs(int[] info, int cnt, int n) {
-        if(cnt == n+1) {
-            int apeach_point = 0;
-            int lion_point = 0;
-            for(int i = 0; i <= 10; i++) 
-            {
-                if(info[i] != 0 || lion[i] != 0) {
-                    if(info[i] < lion[i]) 
-                        lion_point += 10 - i;
-                    else 
-                        apeach_point += 10 - i;
-                }
-            }
-            if(lion_point > apeach_point) {
-                if(lion_point - apeach_point >= max)
-                {
-                    res = lion.clone();
-                    max = lion_point - apeach_point;
-                }
-            }
-            return ;
+    private static int[] lion = {-1};
+    private static int max = Integer.MIN_VALUE;
+    private static int[] res = new int[11];//라이언이 쏜 화살 배열 
+    //점수차 구하기 
+    private static int score(int[] info){
+        int apeach = 0, lion = 0;
+        
+        for(int i=0; i<res.length; i++){
+            if(info[i]==0&&res[i]==0) continue;
+            if(info[i]>=res[i]) apeach += (10-i);
+            else lion += (10-i );
         }
-        for(int j = 0; j <= 10 && lion[j] <= info[j]; j++) {
-            lion[j]++;
-            dfs(info, cnt + 1, n);
-            lion[j]--;
-        }
+        int diff = lion - apeach;
+        if(diff<=0) return -1;
+        return diff;
     }
+    
+    //DFS 백트래킹
+    private static void DFS(int lv, int n, int[] info){
+        //깊이 n까지 오면 
+        if(lv == n){
+            //계산해서 점수 차 
+            int diff = score(info);
+            if(max <= diff){
+                max = diff;
+                lion = res.clone();//복사
+            }
+            return;
+        }
+        for(int i=0; i<info.length && res[i] <= info[i]; i++){
+            res[i]++;
+            DFS(lv+1, n, info);
+            res[i]--;
+        }
+        
+    }
+    
+    //솔루션 함수 
     public int[] solution(int n, int[] info) {
-        lion = new int[11];
-        dfs(info,1,n);
-        return res;
+        DFS(0, n, info);
+        
+        if(max == -1){//라이언이 어피치를 못 이길 경우
+            lion = new int[1];
+            lion[0] = -1;
+        }
+            
+        return lion;
     }
 }
