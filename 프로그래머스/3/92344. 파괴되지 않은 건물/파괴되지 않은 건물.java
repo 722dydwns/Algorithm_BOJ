b584cd6da49b175014614004a67b44a9a12331de
722dydwns <39732720+722dydwns@users.noreply.github.com>
2024-03-02 17:18:38 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 47.36 ms, Memory: 219 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -1,39 +1,47 @@
+import java.util.*;
 class Solution {
+    //솔루션 함수 
     public int solution(int[][] board, int[][] skill) {
-        int n = board.length;
-        int m = board[0].length;
-        //1씩 더 크게 
-        int[][] sums = new int[n+1][m+1];
+        int N = board.length;
+        int M = board[0].length;
+        
+        //누적합용 
+        int[][] sums = new int[N+1][M+1];
         
         for(int[] sk : skill){
-            int degree = sk[0] == 1 ? -sk[5] : sk[5];//공격,회복 여부에 따라 
-            int sr = sk[1], sc = sk[2], er = sk[3], ec = sk[4];
+            int val = sk[0] == 1 ? -sk[5] : sk[5];
+            int sx = sk[1];
+            int sy = sk[2];
+            int ex = sk[3];
+            int ey = sk[4];
             
-            sums[sr][sc] += degree;//누적 
-            sums[sr][ec+1] += (-degree);//반대 부호로 
-            
-            sums[er+1][sc] += -degree;
-            sums[er+1][ec+1] += degree;
+            //영역별 구간합 둘 좌표 찍어두기 
+            sums[sx][sy] += val;
+            sums[sx][ey+1] -= val;
+            sums[ex+1][sy] -= val;
+            sums[ex+1][ey+1] += val;
         }
         
-        for(int c= 0; c<m; ++c){
-            for(int r=  1; r<n; ++r){
-                sums[r][c] += sums[r-1][c];
+        //누적합
+        for(int j = 0; j<M; ++j){
+            for(int i=1; i<N; ++i){
+                sums[i][j] += sums[i-1][j]; //행 기준 직전합에 누적합
             }
         }
         
-        for(int r=0; r<n; ++r){
-            for(int c=1; c<m; ++c){
-                sums[r][c] += sums[r][c-1];
+        for(int i=0; i<N; ++i){
+            for(int j=1; j<M; ++j){
+                sums[i][j] += sums[i][j-1]; //열 기준 직전열에 누적합
             }
         }
         
+        //누적된 sums값을 board에 합체시키기 
         int answer = 0;
         
-        for(int r=0; r<n; ++r){
-            for(int c=0; c<m; ++c){
-                board[r][c] += sums[r][c];
-                if(board[r][c] > 0) answer++;
+        for(int i=0; i<N; ++i){
+            for(int j=0; j<M; ++j){
+                board[i][j] += sums[i][j];
+                if(board[i][j] > 0) answer++;
             }
         }
         
