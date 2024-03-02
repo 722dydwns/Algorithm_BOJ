49c3e3b1caa52a7d4a54117a9ae63e86f840ea61
722dydwns <39732720+722dydwns@users.noreply.github.com>
2024-03-02 15:18:57 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 47.75 ms, Memory: 218 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -1,55 +1,42 @@
-import java.util.*;
-
 class Solution {
-    
-    public static int[][] sums;
-    
     public int solution(int[][] board, int[][] skill) {
+        int n = board.length;
+        int m = board[0].length;
+        //1씩 더 크게 
+        int[][] sums = new int[n+1][m+1];
+        
+        for(int[] sk : skill){
+            int degree = sk[0] == 1 ? -sk[5] : sk[5];//공격,회복 여부에 따라 
+            int sr = sk[1], sc = sk[2], er = sk[3], ec = sk[4];
+            
+            sums[sr][sc] += degree;//누적 
+            sums[sr][ec+1] += (-degree);//반대 부호로 
+            
+            sums[er+1][sc] += -degree;
+            sums[er+1][ec+1] += degree;
+        }
+        
+        for(int c= 0; c<m; ++c){
+            for(int r=  1; r<n; ++r){
+                sums[r][c] += sums[r-1][c];
+            }
+        }
+        
+        for(int r=0; r<n; ++r){
+            for(int c=1; c<m; ++c){
+                sums[r][c] += sums[r][c-1];
+            }
+        }
+        
         int answer = 0;
-        int r = board.length;
-        int c = board[0].length;
-        sums = new int[r+1][c+1];
         
-        for(int[] s:skill){
-            int type = s[0];
-            if(type == 1){
-                sums[s[1]][s[2]] -= s[5];
-                sums[s[3]+1][s[2]] += s[5];
-                sums[s[1]][s[4]+1] += s[5];
-                sums[s[3]+1][s[4]+1] -= s[5];
-            } else {
-                sums[s[1]][s[2]] += s[5];
-                sums[s[3]+1][s[2]] -= s[5];
-                sums[s[1]][s[4]+1] -= s[5];
-                sums[s[3]+1][s[4]+1] += s[5];
-            }
-        }
-        
-
-        // 위 -> 아래
-        for(int j=0;j<c+1;j++){
-            for(int i=0;i<r;i++){
-                sums[i+1][j] += sums[i][j];
-            }
-        }
-        
-
-        // 왼 -> 오른
-        for(int i=0;i<r+1;i++){
-            for(int j=0;j<c;j++){
-                sums[i][j+1] += sums[i][j];
-            }
-        }
-        
-        
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                if(board[i][j] + sums[i][j] > 0) answer++;
+        for(int r=0; r<n; ++r){
+            for(int c=0; c<m; ++c){
+                board[r][c] += sums[r][c];
+                if(board[r][c] > 0) answer++;
             }
         }
         
         return answer;
     }
-    
-
 }
