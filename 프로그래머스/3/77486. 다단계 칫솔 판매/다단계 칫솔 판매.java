32519fa5caed54b7eaa2afca9cea390c0b404541
722dydwns <39732720+722dydwns@users.noreply.github.com>
2024-03-06 20:35:53 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 15.31 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -6,7 +6,7 @@ class Solution {
     private void DFS(int idx, int price){
         int referral = price / 10;
         answer[idx] += price - referral;
-
+ 
         if (parent[idx] != -1 && referral > 0) {
             DFS(parent[idx], referral);
         }
@@ -15,12 +15,12 @@ class Solution {
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
         int N = enroll.length;
         answer = new int[N];
-
+ 
         Map<String, Integer> map = new HashMap<>();
         for(int i=0; i<N; i++){
             map.put(enroll[i], i);
         }
-
+ 
         //부모 세팅
         parent = new int[N];
         for(int i=0; i<N; i++){
@@ -30,14 +30,14 @@ class Solution {
                 parent[i] = -1;
             }
         }
-
+ 
         for(int i=0; i<seller.length; i++){
             int cur_price = amount[i] * 100;
             int cur_idx = map.get(seller[i]);
-
+ 
             DFS(cur_idx, cur_price);
         }
-
+ 
         return answer;
     }
 }
