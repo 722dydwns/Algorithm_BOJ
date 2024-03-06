461c265c1f9509dc8c2c1f26786a0398461c48bb
722dydwns <39732720+722dydwns@users.noreply.github.com>
2024-03-06 15:27:20 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 64.82 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -1,37 +1,39 @@
 import java.util.*;
-class Solution {    
-    public int solution(int[] q1, int[] q2) {
-        int answer = 0;
+
+class Solution {
+    public int solution(int[] queue1, int[] queue2) {
+        Queue<Long> Q1 = new LinkedList<>();
+        Queue<Long> Q2 = new LinkedList<>();
         
-        long total = 0; // 두 큐의 합
-        long q1sum = 0; // 1번 큐의 합
-        Queue<Integer> qq1 = new LinkedList<Integer>();
-        Queue<Integer> qq2 = new LinkedList<Integer>();
-        
-        for(int i = 0; i < q1.length; i++) {
-            total += q1[i] + q2[i];
-            q1sum += q1[i];
-            qq1.add(q1[i]);
-            qq2.add(q2[i]);
+        long sum1 = 0;
+        long total = 0;
+        //두 큐 사이즈가 같기 때문에 한번의 순회로 합과 Q에 add 처리 가능 
+        for(int i=0; i<queue1.length; i++){
+            total += (queue1[i] + queue2[i]);
+            Q1.offer(Long.valueOf(queue1[i]));
+            Q2.offer(Long.valueOf(queue2[i]));
+            sum1 += queue1[i];
         }
+
+		if( total % 2 != 0) return -1;
         
-        if(total%2!=0) return -1; //만약 두큐의합이 홀수면 같게 못만듦.
+        long target = total / 2; //타겟 값 
         
-        long target = total/2;
-        while(true) {
-            if(answer>(q1.length+q2.length)*2) return -1;//이땐 더 순회해도 못 만듦
-            
-            if(q1sum==target) break; // 한쪽의 큐 합을 계산했을 때 전체의 반이라면 종료한다.
-            else if(q1sum>target){   // q1sum이 target보다 크다면 
-                q1sum -= qq1.peek(); // 큐1 합에는 큐2의 것을 빼고
-                qq2.add(qq1.poll()); // 큐2에 큐1의 것을 넣어줍니다.
-            }else{                   // q2sum이 target보다 작다면
-                q1sum += qq2.peek(); // 큐1 합에는 큐2의 것을 더하고
-                qq1.add(qq2.poll()); // 큐1에 큐2의 것을 넣어줍니다.
+        int count = 0;
+				
+        while(true){
+	        // 넉넉 잡아 두 바퀴를 돌아도 안되면 안됨
+            if(count > (queue1.length+queue2.length) *2) return -1;
+            if(sum1 == target) break;
+            if(sum1 > target){
+                sum1 -= Q1.peek();
+                Q2.offer(Q1.poll());
+            }else{
+                sum1 += Q2.peek();
+                Q1.offer(Q2.poll());
             }
-            answer++;
+            count++;
         }
-        
-        return answer;
+        return count;
     }
 }
