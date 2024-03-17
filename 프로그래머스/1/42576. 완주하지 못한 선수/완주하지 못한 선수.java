ebf135935007d2c27183a8afb38f10a62d25cfb1
722dydwns <39732720+722dydwns@users.noreply.github.com>
2024-03-17 17:33:04 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 84.07 ms, Memory: 96.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+    
+        HashMap<String ,Integer> map = new HashMap<>();
+        
+        for(String x : participant){
+            map.put(x, map.getOrDefault(x, 0)+1);
+        }
+        
+        for(String x : completion){
+            map.put(x, map.getOrDefault(x, 0)-1);
+        }
+        
+        String answer = "";
+        
+        for(String key : map.keySet()){
+            if(map.get(key) != 0){
+                answer = key;
+            }
+        }
+        
+        
+        return answer;
+    }
+}
