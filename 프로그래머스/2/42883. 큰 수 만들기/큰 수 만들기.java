2e9a4ec584f25a699ada52b7f94db82e7adba0cd
722dydwns <39732720+722dydwns@users.noreply.github.com>
2024-03-27 13:14:25 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.03 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,19 +1,25 @@
 import java.util.*;
 class Solution {
+    //솔루션 함수 
     public String solution(String number, int k) {
+       
         StringBuilder sb = new StringBuilder();
-        int index = 0;
-        int max = 0;
-        for(int i=0; i<number.length() - k; i++) {
-            max = 0;
-            for(int j = index; j<= k+i; j++) {
-                if(max < number.charAt(j)-'0') {
-                    max = number.charAt(j)-'0';
-                    index = j+1;
+
+        
+        int st = 0;
+        
+        for(int i=0; i<number.length() - k; i++){ //i로 밀고 가는 건 최대 k뺀 값까지 
+            char max = '0';
+            for(int j=st; j<= i + k; j++){
+                if(max < number.charAt(j)){
+                    max = number.charAt(j);
+                    st = j + 1;
                 }
             }
-            sb.append(max);
+            //여기서 max값 이어붙임 
+            sb.append(max);   
         }
+        
         return sb.toString();
     }
 }
