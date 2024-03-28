9c5a58b32497ffc7a2acc634482a07fc68ede72d
722dydwns <39732720+722dydwns@users.noreply.github.com>
2024-03-28 12:53:44 +0900

    [level 2] Title: [3차] 방금그곡, Time: 3.89 ms, Memory: 65 MB -BaekjoonHub

--- 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
@@ -20,7 +20,7 @@ class Solution {
         String answer ="(None)";
         int maxPlayTime = -1;
         m = changeMelody(m);
-
+        System.out.println("m : "+ m);
         for(String x : musicinfos){
             String[] tmp = x.split(",");
             
@@ -30,7 +30,7 @@ class Solution {
             
             String name = tmp[2];
             String melodyInfo = changeMelody(tmp[3]);
-          
+            System.out.println("멜로디: " + melodyInfo);
             //재생 시간 > 음악 길이
             if(time > melodyInfo.length()){
                 StringBuilder newMel = new StringBuilder();
@@ -41,8 +41,10 @@ class Solution {
                 //나머지 만큼 악보에서 잘라 붙임
                 newMel.append(melodyInfo.substring(0, time % melodyInfo.length()));
                 melodyInfo = newMel.toString();
+                System.out.println("바꿈 " + melodyInfo);
             }else{
                 melodyInfo= melodyInfo.substring(0, time);
+                System.out.println("바꿈 " + melodyInfo);
             }
         
             if(melodyInfo.contains(m) && time > maxPlayTime){
