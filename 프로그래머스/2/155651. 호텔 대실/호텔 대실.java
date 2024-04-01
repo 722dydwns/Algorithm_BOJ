e1faa40af138dc934dac08f3ca711ad8d28e4b57
722dydwns <39732720+722dydwns@users.noreply.github.com>
2024-04-01 17:41:32 +0900

    [level 2] Title: 호텔 대실, Time: 19.65 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -1,58 +1,61 @@
 import java.util.*;
 
+class Room implements Comparable<Room>{
+    int st, ed;
+    Room(int st, int ed){
+        this.st = st;
+        this.ed = ed;
+    }
+    @Override
+    public int compareTo(Room o){
+        // if(this.ed == o.ed) return this.st - o.st;
+        // return this.ed - o.ed;//기본 끝 시간 오름차순 정렬 
+        
+        
+        if (this.st != o.st) return this.st - o.st; // 1 - 시작 시간이 빠른 순서로 정렬
+        return this.ed - o.ed; // 2 - 끝나는 시간이 빠른 순서로 정렬
+    }
+}
+
 class Solution {
-    class Book{
-        public Book(int start_time, int end_time) {
-            this.start_time = start_time;
-            this.end_time = end_time;
-        }
-
-        int start_time,end_time;
+    //getTime
+    public static int getTime(String time){
+        int H = Integer.parseInt(time.split(":")[0]);
+        int M = Integer.parseInt(time.split(":")[1]);
+        
+        return 60 * H + M;
     }
-
-    List<Book> bookList=new ArrayList<>();
-    int toMinute(String time){
-        StringTokenizer stk=new StringTokenizer(time,":");
-        int hour=Integer.parseInt(stk.nextToken());
-        int minute=Integer.parseInt(stk.nextToken());
-        return hour*60+minute;
-    }
-
-    public int solution(String[][] book_times) {
-        for(String[] book_time:book_times){
-            int start_time=toMinute(book_time[0]);
-            int end_time=toMinute(book_time[1]);
-
-            bookList.add(new Book(start_time,end_time));
+    
+    //솔루션 함수 
+    public int solution(String[][] book_time) {
+        int answer = 0;
+        int N = book_time.length;//방 개수 
+        List<Room> roomList = new ArrayList<>();
+  
+        for(String[] tmp : book_time){
+            int st = getTime(tmp[0]);
+            int ed= getTime(tmp[1]);
+            roomList.add(new Room(st, ed));
         }
-        // 시작 시간대로 정렬
-        Collections.sort(bookList,(o1,o2)->{
-            if(o1.start_time==o2.start_time) return o1.end_time-o2.end_time;
-            else return o1.start_time-o2.start_time;
-        });
-
-        // 리스트를 방문하며 몇개가 필요할지 체크
-        List<Integer> endTimeList=new ArrayList<>();
-
-        for(Book book:bookList){
-            boolean isOk=false;
-            // end_time 기준으로 정렬
-            Collections.sort(endTimeList);
-            for(int i=0;i<endTimeList.size();i++){
-                // 정리시간 10분
-                int endTime=endTimeList.get(i)+10;
-                // check
-                if(book.start_time>=endTime) {
-                    // 예약 시간 넣고 업데이트
-                    endTimeList.set(i,book.end_time);
-                    isOk=true;
-                    break;
+        //ed 시간 오름차순 정렬
+        Collections.sort(roomList);
+        
+        boolean[] visited= new boolean[N];
+        for(int i=0; i<roomList.size(); i++){
+            Room cur = roomList.get(i);
+            if(visited[i]) continue;
+            visited[i] = true;
+            answer++;
+            
+            System.out.println(i);
+            
+            for(int j=i+1; j<roomList.size(); j++){
+                if(cur.ed + 10 <= roomList.get(j).st && !visited[j]){ // 한번 체크했던 room은 넘어가도록 !visited[j] 추가
+                    visited[j] = true;
+                    cur = roomList.get(j);
                 }
             }
-            // 아무 방도 넣지 못하면 새로운 방 추가
-            if(!isOk) endTimeList.add(book.end_time);
         }
-
-        return endTimeList.size();
+        return answer;
     }
 }
