86039466003236941342e4a827c11c14bb769aec
722dydwns <39732720+722dydwns@users.noreply.github.com>
2024-04-03 12:55:18 +0900

    [level 2] Title: 호텔 대실, Time: 21.82 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -1,61 +1,48 @@
 import java.util.*;
-
 class Room implements Comparable<Room>{
     int st, ed;
     Room(int st, int ed){
         this.st = st;
         this.ed = ed;
     }
-    @Override
     public int compareTo(Room o){
-        // if(this.ed == o.ed) return this.st - o.st;
-        // return this.ed - o.ed;//기본 끝 시간 오름차순 정렬 
-        
-        
-        if (this.st != o.st) return this.st - o.st; // 1 - 시작 시간이 빠른 순서로 정렬
-        return this.ed - o.ed; // 2 - 끝나는 시간이 빠른 순서로 정렬
+        if(this.st == o.st) return this.ed - o.ed;//시작값 같으면 e 오름차순
+        return this.st-o.st;//기본 시작 시간 오름차순 정렬 
     }
 }
-
 class Solution {
-    //getTime
-    public static int getTime(String time){
-        int H = Integer.parseInt(time.split(":")[0]);
-        int M = Integer.parseInt(time.split(":")[1]);
-        
+    public int getTime(String x){
+        int H = Integer.parseInt(x.split(":")[0]);
+        int M = Integer.parseInt(x.split(":")[1]);
         return 60 * H + M;
     }
-    
     //솔루션 함수 
     public int solution(String[][] book_time) {
         int answer = 0;
-        int N = book_time.length;//방 개수 
+        
         List<Room> roomList = new ArrayList<>();
-  
-        for(String[] tmp : book_time){
-            int st = getTime(tmp[0]);
-            int ed= getTime(tmp[1]);
+        for(String[] x : book_time){
+            int st = getTime(x[0]);
+            int ed = getTime(x[1]);
             roomList.add(new Room(st, ed));
         }
-        //ed 시간 오름차순 정렬
-        Collections.sort(roomList);
         
-        boolean[] visited= new boolean[N];
+        Collections.sort(roomList);
+        boolean[] visited= new boolean[book_time.length];
+        //시작시간 오름차순 기준 
         for(int i=0; i<roomList.size(); i++){
-            Room cur = roomList.get(i);
             if(visited[i]) continue;
-            visited[i] = true;
+            Room target = roomList.get(i);
+            visited[i] =true;
             answer++;
-            
-            System.out.println(i);
-            
             for(int j=i+1; j<roomList.size(); j++){
-                if(cur.ed + 10 <= roomList.get(j).st && !visited[j]){ // 한번 체크했던 room은 넘어가도록 !visited[j] 추가
+                if(!visited[j] && roomList.get(j).st >= target.ed + 10){
                     visited[j] = true;
-                    cur = roomList.get(j);
+                    target = roomList.get(j);
                 }
             }
         }
+        
         return answer;
     }
 }
