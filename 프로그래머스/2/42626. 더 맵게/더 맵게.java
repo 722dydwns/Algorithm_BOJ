2619ab542503a44468d1400aba11d1dfe12682c3
722dydwns <39732720+722dydwns@users.noreply.github.com>
2024-04-15 11:34:06 +0900

    [level 2] Title: 더 맵게, Time: 1838.01 ms, Memory: 130 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,30 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        Integer answer = 0;
+        Integer one, two;
+        PriorityQueue<Integer> queue = new PriorityQueue<Integer>();//큐 생성
+
+        //할당
+        for (int i = 0; i < scoville.length; i++) {
+            queue.add(scoville[i]);
+        }
+
+        while(queue.peek() < K) {
+            //int형 우선순위가 낮은 숫자 순
+            if (queue.size() < 2) {
+                return -1;
+            }
+            else {
+                one = queue.poll();
+                two = queue.poll();
+
+                one += (two * 2);
+                queue.offer(one);
+                answer += 1;
+            }
+        }
+        return answer;
+    }
+}
