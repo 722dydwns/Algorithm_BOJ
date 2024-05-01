e47d48b28a397ed9eb1424db3810afa9e7e47146
722dydwns <39732720+722dydwns@users.noreply.github.com>
2024-05-01 10:38:51 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 83.84 ms, Memory: 133 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -1,12 +1,26 @@
-import java.util.Arrays;
- 
-public class Solution {
+import java.util.*;
 
-    public static long[] solution(long[] numbers) {
-        long[] answer = numbers.clone();
-        for (int i = 0; i < numbers.length; i++) {
-            answer[i]++; // x보다 큰수로 만든다.
-            answer[i] += (answer[i] ^ numbers[i]) >> 2;
+class Solution {
+    public long[] solution(long[] numbers) {
+        long[] answer = new long[numbers.length];
+        
+        for (int i = 0; i<numbers.length; i++) {
+            long result = numbers[i];
+            String target = Long.toBinaryString(numbers[i]);
+            
+            if (result % 2 == 0) {
+                answer[i] = result + 1;
+            } else {
+                int idx = target.lastIndexOf("0");
+                
+                if (idx == -1) {
+                    String tmp = "10" + target.substring(1, target.length());
+                    answer[i] = Long.parseLong(tmp, 2);
+                } else {
+                    String tmp = target.substring(0, idx) + "10" + target.substring(idx + 2, target.length());
+                    answer[i] = Long.parseLong(tmp, 2);
+                }
+            }
         }
         return answer;
     }
