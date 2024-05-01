7b28d66e6e35659265fac147f775b85a362861aa
722dydwns <39732720+722dydwns@users.noreply.github.com>
2024-05-01 11:00:20 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 121.47 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -1,27 +1,29 @@
 import java.util.*;
 
 class Solution {
-    public long[] solution(long[] numbers) {
-        long[] answer = new long[numbers.length];
+    //솔루션 함수 
+    public List<Long> solution(long[] numbers) {
+        List<Long> answer = new ArrayList<>();
         
-        for (int i = 0; i<numbers.length; i++) {
-            long result = numbers[i];
-            String target = Long.toBinaryString(numbers[i]);
+        for(long num : numbers){
+            String target = Long.toBinaryString(num);//이진수 변환 
             
-            if (result % 2 == 0) {
-                answer[i] = result + 1;
-            } else {
+            if(num % 2 == 0){ //현재 수가 짝수
+                //1더한 값을 변환한 게 정답
+                answer.add(num+1);
+            }else{ //현재 수가 홀수 
                 int idx = target.lastIndexOf("0");
-                
-                if (idx == -1) {
+                if(idx == -1){//없다면 맨 앞에 10 붙임
                     String tmp = "10" + target.substring(1, target.length());
-                    answer[i] = Long.parseLong(tmp, 2);
-                } else {
-                    String tmp = target.substring(0, idx) + "10" + target.substring(idx + 2, target.length());
-                    answer[i] = Long.parseLong(tmp, 2);
+                    answer.add(Long.parseLong(tmp, 2)); 
+                }else{
+                    String tmp = target.substring(0, idx) + "10" + target.substring(idx+2, target.length());
+                    answer.add(Long.parseLong(tmp, 2));
                 }
-            }
+                
+            }            
         }
+
         return answer;
     }
 }
